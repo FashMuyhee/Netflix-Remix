5425ff1e2774f1be2bf4acfb0b412c1c549070ec
fashmuyhee <fashmuyhee@gmail.com>
2021-01-09 03:23:43 +0100

    added slider  component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import {View} from 'react-native';
-import {Text} from 'react-native-paper';
-import { ScrollContainer,MyText } from '../components';
+import React, {useState} from 'react';
+import {Image, StyleSheet} from 'react-native';
+import {Avatar} from 'react-native-paper';
+import {
+  ScrollContainer,
+  NavBar,
+  BigMovieCard,
+  SmallMovieCard,
+  ListHeader,ContinueWatchingCard
+} from '../components';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 const Home = () => {
+  const [state, setState] = useState([
+    {title: 'Movie 1'},
+    {title: 'Movie 2'},
+    {title: 'Movie 3'},
+  ]);
+
   return (
     <>
+      <NavBar
+        left={<Icon name="menu" color="white" size={25} />}
+        center={<Image source={require('../assets/img/logo.png')} />}
+        right={
+          <Avatar.Image
+            source={require('../assets/img/avatar.png')}
+            size={40}
+          />
+        }
+      />
       <ScrollContainer>
-        <MyText>Home Boy</MyText>
+        <BigMovieCard data={state} />
+        <ListHeader title="continue watching" />
+        <ContinueWatchingCard data={state} />
+        <ListHeader title="my list" />
+        <SmallMovieCard data={state} />
+        <ListHeader title="netflix originals" />
+        <SmallMovieCard data={state} />
       </ScrollContainer>
     </>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({});
