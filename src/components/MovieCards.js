113bedd5788fc1af7eb62db2f1b78121d7669545
fashmuyhee <fashmuyhee@gmail.com>
2021-01-09 03:23:12 +0100

    restructure for continue_watcihng

--- src/components/MovieCards.js
+++ src/components/MovieCards.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, TouchableWithoutFeedback, StyleSheet} from 'react-native';
 import {Text, Colors} from 'react-native-paper';
 import Carousel from 'react-native-snap-carousel';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import {useNavigation} from '@react-navigation/native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 const BigMovieCard = ({data}) => {
+  const navigation = useNavigation();
   const _renderItem = ({item, index}) => {
     return (
-      <View
-        key={index}
-        style={{
-          backgroundColor: Colors.red600,
-          height: 150,
-          borderRadius: 10,
-          padding: 20,
-        }}>
-        <Text>{item.title}</Text>
-      </View>
+      <TouchableWithoutFeedback onPress={() => navigation.navigate('movie')}>
+        <View
+          key={index}
+          style={{
+            backgroundColor: Colors.red600,
+            height: 150,
+            borderRadius: 10,
+            padding: 20,
+          }}>
+          <Text>{item.title}</Text>
+        </View>
+      </TouchableWithoutFeedback>
     );
   };
   return (
@@ -40,25 +46,35 @@ const BigMovieCard = ({data}) => {
       layout="default"
       layoutCardOffset={2}
       enableMomentum={true}
-      onTouchEnd={() => console.log('pressed')}
     />
   );
 };
 
-const SmallMovieCard = ({data}) => {
+const ContinueWatchingCard = ({data}) => {
+  const navigation = useNavigation();
+
   const _renderItem = ({item, index}) => {
     return (
-      <View
-        key={index}
-        style={{
-          backgroundColor: Colors.blue700,
-          height: hp(20),
-          borderRadius: 10,
-          padding: 20,
-          marginRight: 5,
-        }}>
-        <Text>{item.title}</Text>
-      </View>
+      <TouchableWithoutFeedback onPress={() => navigation.navigate('movie')}>
+        <View
+          key={index}
+          style={{
+            backgroundColor: Colors.blueA400,
+            height: hp(20),
+            borderRadius: 10,
+            marginRight: 5,
+          }}>
+          <View style={styles.movieImage}></View>
+          <View>
+            <View style={styles.progressBarFull} />
+            <View style={styles.progressBarCurrent} />
+            <View style={styles.details}>
+              <Text>{item.title}</Text>
+              <FeatherIcon name="play-circle" color="white" size={26} />
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
     );
   };
   return (
@@ -82,4 +98,68 @@ const SmallMovieCard = ({data}) => {
     />
   );
 };
-export {BigMovieCard, SmallMovieCard};
+const SmallMovieCard = ({data}) => {
+  const navigation = useNavigation();
+
+  const _renderItem = ({item, index}) => {
+    return (
+      <TouchableWithoutFeedback onPress={() => navigation.navigate('movie')}>
+        <View
+          key={index}
+          style={{
+            backgroundColor: Colors.blueA400,
+            height: hp(20),
+            borderRadius: 10,
+            marginRight: 5,
+          }}></View>
+      </TouchableWithoutFeedback>
+    );
+  };
+  return (
+    <Carousel
+      data={data}
+      renderItem={_renderItem}
+      sliderWidth={wp(100)}
+      itemWidth={wp(27)}
+      containerCustomStyle={{
+        padding: 10,
+        height: hp(24),
+        marginTop: 20,
+        marginBottom: 20,
+      }}
+      layout="default"
+      activeAnimationType="spring"
+      inactiveSlideScale={0.94}
+      inactiveSlideOpacity={1}
+      loop={true}
+      loopClonesPerSide={2}
+    />
+  );
+};
+export {BigMovieCard, SmallMovieCard, ContinueWatchingCard};
+
+const styles = StyleSheet.create({
+  progressBarFull: {
+    backgroundColor: Colors.lightGreenA400,
+    height: 4,
+  },
+  progressBarCurrent: {
+    backgroundColor: Colors.redA100,
+    height: 4,
+    width: '40%',
+    position: 'absolute',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+  },
+  movieImage: {
+    backgroundColor: Colors.white,
+    height: '70%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+});
